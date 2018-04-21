77ab3f3db012f9b4f8b02d5aadf5344b2a9012b3
florian-vuillemot <florian.vuillemot@epitech.eu>
2018-04-21 18:16:25 +0200

    Update the entry point with new method

--- index.py
+++ index.py
@@ -1,127 +1,72 @@
-
-# coding: utf-8
-
-# In[54]:
-
-
-import json
-from typing import List, Tuple, Dict
-
-
-# In[49]:
-
-
-TIME = 30
-
-
-# In[56]:
-
-
-def load_json() -> List[Dict[str, str]]:
-    with open("zipcode-belgium.json") as f:
-        return json.loads(f.read())
-
-
-# In[119]:
-
-
-def filter_city() -> Dict[str, str]:
-    res = {}
-    for c in load_json():
-        del c['zip']
-        city = c['city']
-        del c['city']
-        res[city] = c
-    return res
-
-
-# In[58]:
-
-
-from math import radians, cos, sin, asin, sqrt
-
-def _haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
-
-def haversine(c1: Dict[str, str], c2: Dict[str, str]) -> float:
-    return _haversine(c1['lng'], c1['lat'], c2['lng'], c2['lat'])
-
-
-# In[59]:
-
-
-def get_time(c1: Dict[str, str], c2: Dict[str, str]) -> float:
-    d = haversine(c1, c2)
-    return d * 2
-
-
-# In[63]:
-
-
 import copy
-def get_all_distances(start: str, cities: Dict[str, str]) -> List[Tuple[str, float]]:
-    _cities = copy.deepcopy(cities)
-    del _cities[start]
-    distances = [(name, get_time(cities[start], points)) for (name, points) in _cities.items()]
-    sorted(distances, key=lambda x: x[1])
-    return distances
-
-
-# In[66]:
-
-
-def keep_points(points: List[Tuple[str, float]], t: float) -> List[Tuple[str, float]]:
-    return [point for point in points if point[1] < t]
-
-
-# In[127]:
-
-
 import random
-def select_first_point(points: List[Tuple[str, float]]) -> Tuple[str, float]:
-    _points = keep_points(points, TIME / 3)
-#    return _points[0]
-    ipoint = random.randint(0, len(_points) - 1)
-    return _points[ipoint]
+from typing import Iterable, Tuple, List
+from cities.city import get_cities
+from distances.helpers import distances_from_point, keep_in_circle,\
+    DistanceCalculator
+from distances.haversine.haversine import haversine, time_estimation
 
 
-# In[128]:
+def get_random_path(ref_city: 'City', cities: List['City'],
+                    distance: float,
+                    distance_calculator: DistanceCalculator)\
+                    -> Tuple['City', 'City']:
+    '''
+    Create a random path by selecting 2 cities
+    This cities is at equal distance each of then and make a triangle
+    '''
+    _distance = distance / 3
+    distances_ref = list(keep_in_circle(reference_point=ref_city,
+                                        points=cities,
+                                        distance_calculator=distance_calculator,
+                                        radius=_distance))
+    first_city = get_random_city(distances_ref)
+    other_cities = extract_city(first_city.name, cities)[1]
+    distances_first = list(keep_in_circle(reference_point=first_city,
+                                          points=other_cities,
+                                          distance_calculator=distance_calculator,
+                                          radius=_distance))
+    second_city = _get_second_city(distances_ref, distances_first)
+    return (first_city, second_city)
 
 
-def select_second_point(start: str, points: Dict[str, float],
-                        first_point: Tuple[str, str], distance_start_points: Dict[str, float]):
-    distances = get_all_distances(first_point[0], points)
-    distances = keep_points(distances, TIME / 3)
-    res = []
-    for city in distances:
-        for other in distance_start_points:
-            if other[0] == city[0]:
-                res.append((city[0], city[1], other[1]))
+def _get_second_city(l1: List[Tuple[float, 'City']], l2: List[Tuple[float, 'City']])\
+    -> 'City':
+    points = []  # type: List[Tuple[float, 'City']]
+    for i in l1:
+        for j in l2:
+            if i[1] == j[1]:
+                points.append((i[0] + j[0], i, j))
                 break
-    return min(res, key=lambda x: abs(x[1] - x[2]))
-    
+    return max(points, key=lambda x: x[0])[1][1]
+
+def get_random_city(cities_distances: List[Tuple[float, 'City']],
+                    nb_random: int = 10) -> 'City':
+    '''
+    Select the city with the biggest distance from nb_random city select
+    '''
+    rd_cities = random.sample(cities_distances, nb_random)
+    return max(rd_cities, key=lambda c: c[0])[1]
 
 
-# In[131]:
+def extract_city(city_name: str, cities: List['City'])\
+    -> Tuple['City', List['City']]:
+    '''
+    Found the city in cities and return it with other cities
+    '''
+    other_cities = [city for city in cities if city.name != city_name]
+    city = next((city for city in cities if city.name == city_name))
+    return (city, other_cities)
 
 
-cities = filter_city()
-distances = get_all_distances('Bruxelles', cities)
-first_point = select_first_point(distances)
-print(first_point)
-second_point = select_second_point('Bruxelles', cities, first_point, distances)
-print(second_point)
+def d_calculator(**args) -> float:
+    haversine_distance = haversine(**args)
+    return time_estimation(haversine_distance, 30)
 
+
+if __name__ == '__main__':
+    city_name = 'Bruxelles'
+    distance = 1
+    city, cities = extract_city(city_name, get_cities())
+    c1, c2 = get_random_path(city, cities, distance, d_calculator)
+    print(city, c1.name, c2.name)
