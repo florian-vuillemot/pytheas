a2b62a18396032523634062d3663b091d500bdf1
florian-vuillemot <florian.vuillemot@epitech.eu>
2018-04-21 15:30:42 +0200

    Add helper for filter base on radius. Refacto code and TU

--- distances/helpers.py
+++ distances/helpers.py
@@ -1,7 +1,7 @@
 '''
 Helpers methods for make calcul on distances.
 '''
-from typing import List, Dict, Tuple, Union, Any, Callable
+from typing import List, Dict, Tuple, Union, Any, Callable, Iterable
 
 
 GPSPoint = Dict[str, Union[float, Any]]
@@ -9,27 +9,48 @@ GPSPoint = Dict[str, Union[float, Any]]
 DistanceCalculator = Callable[[float, float, float, float], float]
 
 
-def evaluate_all_distance(distances: List[GPSPoint],
-                          distance_calculator: DistanceCalculator,
-                          latitude_key: str = 'latitude',
-                          longitude_key: str = 'longitude')\
-                          -> List[Tuple[float, GPSPoint, GPSPoint]]:
+def evaluate_all_distance(points: List[GPSPoint],
+                          **args)\
+                          -> Iterable[Tuple[float, GPSPoint, GPSPoint]]:
     '''
     Evaluate the distance between each element of the list
     Return a list with each distance between each elements
 
-    Each elements of distances should contain a latitude and longitude key
+    Each elements of points should contain a latitude and longitude key
     '''
-    res = []  # type: List[Tuple[float, GPSPoint, GPSPoint]]
+    for i in range(len(points)):
+        pnt = points[i]
+        for (d, oth) in (distances_from_point(pnt, points[i + 1:], **args)):
+            yield (d, pnt, oth)
 
-    for i in range(len(distances)):
-        d1 = distances[i]
-        for j in range(i + 1, len(distances)):
-            d2 = distances[j]
-            distance = distance_calculator(lat1=d1[latitude_key],
-                                           lat2=d2[latitude_key],
-                                           lon1=d1[longitude_key],
-                                           lon2=d2[longitude_key])
-            res.append((distance, d1, d2))
 
-    return res
+def keep_in_circle(reference_point: GPSPoint,
+                   points: List[GPSPoint],
+                   radius: float,
+                   **args)\
+                   -> Iterable[Tuple[float, GPSPoint]]:
+    '''
+    Keep all element points in a circle around the reference point
+    '''
+    for (d, oth) in (distances_from_point(reference_point, points, **args)):
+        if d <= radius:
+            yield (d, reference_point, oth)
+
+
+def distances_from_point(reference: GPSPoint,
+                         points: List[GPSPoint],
+                         distance_calculator: DistanceCalculator,
+                         latitude_key: str = 'latitude',
+                         longitude_key: str = 'longitude')\
+                         -> Iterable[Tuple[(float, GPSPoint)]]:
+    '''
+    Return distance of each point from reference
+    '''
+    # This method is not test directly
+    # Test though other method
+    for point in points:
+        distance = distance_calculator(lat1=reference[latitude_key],
+                                       lon1=reference[longitude_key],
+                                       lat2=point[latitude_key],
+                                       lon2=point[longitude_key])
+        yield (distance, point)
--- distances/test_helpers.py
+++ distances/test_helpers.py
@@ -1,9 +1,9 @@
 import unittest
-from helpers import evaluate_all_distance
+from helpers import evaluate_all_distance, keep_in_circle
 
 
 class TestHelpers(unittest.TestCase):
-    distances = [
+    points = [
         {'id': 0, 'lat': 1, 'lon': 1},
         {'id': 1, 'lat': 2, 'lon': 2},
         {'id': 2, 'lat': 3, 'lon': 3},
@@ -13,13 +13,13 @@ class TestHelpers(unittest.TestCase):
 
     def test_evaluate_all_distance(self):
         results_distances = evaluate_all_distance(
-            distances=self.distances,
+            points=self.points,
             distance_calculator=distance_calculator,
             latitude_key='lat',
             longitude_key='lon'
         )
 
-        id_d1, id_d2, nb_distances = 0, 1, len(self.distances)
+        id_d1, id_d2, nb_distances = 0, 1, len(self.points)
         for distance in results_distances:
             r, d1, d2 = distance
 
@@ -31,10 +31,41 @@ class TestHelpers(unittest.TestCase):
                 id_d1 += 1
                 id_d2 = id_d1 + 1
 
+    def test_keep_in_circle(self):
+        nb_points = len(self.points) - 1
+        params = {
+            'reference_point': self.points[0],
+            'points': self.points[1:],
+            'latitude_key': 'lat',
+            'longitude_key': 'lon'
+        }
+        # Radius <= 2
+        params['distance_calculator'] = radius_distance_eq2_or_eq1
+        self.assertEqual(len(list(keep_in_circle(radius=2, **params))), 2)
+        params['distance_calculator'] = radius_distance_eq3
+        self.assertEqual(len(list(keep_in_circle(radius=1, **params))), 1)
+        params['distance_calculator'] = radius_distance_all_1
+        self.assertEqual(len(list(keep_in_circle(radius=2, **params))), nb_points)
+        params['distance_calculator'] = radius_distance_all_1
+        self.assertEqual(len(list(keep_in_circle(radius=0, **params))), 0)
+
 
 def distance_calculator(lat1: float, lon1: float,
                         lat2: float, lon2: float) -> float:
     return 42.42
+
+
+def radius_distance_eq2_or_eq1(lat1: float, lon1: float,
+                               lat2: float, lon2: float) -> float:
+    return 1 if lat2 == 2 or lat2 == 3 else 50
+
+def radius_distance_eq3(lat1: float, lon1: float,
+                        lat2: float, lon2: float) -> float:
+    return 1 if lat2 == 3 else 50
+
+def radius_distance_all_1(lat1: float, lon1: float,
+                          lat2: float, lon2: float) -> float:
+    return 1
 
 
 if __name__ == '__main__':
